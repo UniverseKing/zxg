9b1842bb421b596a6c88adc9cc0510810cfdae17
chunfeng <chunfeng@zxyi.cn>
2019-11-13 11:02:48 +0800

    '项目四大模块路由搭建'

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
+import home from '@/views/home/home.vue'
+import category from '@/views/category/category.vue'
+import shopcart from '@/views/shopcart/shopcart.vue'
+import user from '@/views/user/user.vue'
+
 const routes = [
-  
+  {
+    path:'/home',
+    component:home
+  },
+  {
+    path:'/category',
+    component:category
+  },
+  {
+    path:'/shopcart',
+    component:shopcart
+  },
+  {
+    path:'/user',
+    component:user
+  }
 ]
 
 const router = new VueRouter({
