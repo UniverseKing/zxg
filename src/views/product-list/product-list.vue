8579151e59d5369c721adc7b9b18c25450c345db
小妖2小仙 <duguzixiao@icloud.com>
2019-11-23 16:18:03 +0800

    商品列表下拉刷新

--- src/views/product-list/product-list.vue
+++ src/views/product-list/product-list.vue
@@ -30,8 +30,54 @@
 
     <!-- 商品列表 -->
     <section class="product-list">
-        <div class="product-container">
-            <van-card v-for='(item,index) in productList' :key='index' :num="item.stock" :price="item.price" :desc="item.subtitle" :title="item.name" :thumb="item.imageHost+item.mainImage" :origin-price="item.originalPrice" @click="goDetail(item.id)" />
+        <div class="product-container" ref="scroller">
+            <scroller :on-refresh="refresh" :on-infinite="infinite" refresh-layer-color="#ec4949" loading-layer-color="#ec4949" ref="myScroller">
+                <!-- custom refresh spinner, use default `spinner` & viewBox 0,0,64,64 class -->
+                <svg class="spinner" style="stroke: #ec4949;" slot="refresh-spinner" viewBox="0 0 64 64">
+                    <g stroke-width="7" stroke-linecap="round">
+                        <line x1="10" x2="10" y1="27.3836" y2="36.4931">
+                            <animate attributeName="y1" dur="750ms" values="16;18;28;18;16;16" repeatCount="indefinite"></animate>
+                            <animate attributeName="y2" dur="750ms" values="48;46;36;44;48;48" repeatCount="indefinite"></animate>
+                            <animate attributeName="stroke-opacity" dur="750ms" values="1;.4;.5;.8;1;1" repeatCount="indefinite"></animate>
+                        </line>
+                        <line x1="24" x2="24" y1="18.6164" y2="45.3836">
+                            <animate attributeName="y1" dur="750ms" values="16;16;18;28;18;16" repeatCount="indefinite"></animate>
+                            <animate attributeName="y2" dur="750ms" values="48;48;46;36;44;48" repeatCount="indefinite"></animate>
+                            <animate attributeName="stroke-opacity" dur="750ms" values="1;1;.4;.5;.8;1" repeatCount="indefinite"></animate>
+                        </line>
+                        <line x1="38" x2="38" y1="16.1233" y2="47.8767">
+                            <animate attributeName="y1" dur="750ms" values="18;16;16;18;28;18" repeatCount="indefinite"></animate>
+                            <animate attributeName="y2" dur="750ms" values="44;48;48;46;36;44" repeatCount="indefinite"></animate>
+                            <animate attributeName="stroke-opacity" dur="750ms" values=".8;1;1;.4;.5;.8" repeatCount="indefinite"></animate>
+                        </line>
+                        <line x1="52" x2="52" y1="16" y2="48">
+                            <animate attributeName="y1" dur="750ms" values="28;18;16;16;18;28" repeatCount="indefinite"></animate>
+                            <animate attributeName="y2" dur="750ms" values="36;44;48;48;46;36" repeatCount="indefinite"></animate>
+                            <animate attributeName="stroke-opacity" dur="750ms" values=".5;.8;1;1;.4;.5" repeatCount="indefinite"></animate>
+                        </line>
+                    </g>
+                </svg>
+
+                <van-card v-for="(item,index) in productList" :key="index" :num="item.stock" :price="item.price" :desc="item.subtitle" :title="item.name" :thumb="item.imageHost + item.mainImage" :origin-price="item.originalPrice" />
+
+                <!-- custom infinite spinner -->
+                <svg class="spinner" style="fill: #ec4949;" slot="infinite-spinner" viewBox="0 0 64 64">
+                    <g>
+                        <circle cx="16" cy="32" stroke-width="0" r="3">
+                            <animate attributeName="fill-opacity" dur="750ms" values=".5;.6;.8;1;.8;.6;.5;.5" repeatCount="indefinite"></animate>
+                            <animate attributeName="r" dur="750ms" values="3;3;4;5;6;5;4;3" repeatCount="indefinite"></animate>
+                        </circle>
+                        <circle cx="32" cy="32" stroke-width="0" r="3.09351">
+                            <animate attributeName="fill-opacity" dur="750ms" values=".5;.5;.6;.8;1;.8;.6;.5" repeatCount="indefinite"></animate>
+                            <animate attributeName="r" dur="750ms" values="4;3;3;4;5;6;5;4" repeatCount="indefinite"></animate>
+                        </circle>
+                        <circle cx="48" cy="32" stroke-width="0" r="4.09351">
+                            <animate attributeName="fill-opacity" dur="750ms" values=".6;.5;.5;.6;.8;1;.8;.6" repeatCount="indefinite"></animate>
+                            <animate attributeName="r" dur="750ms" values="5;4;3;3;4;5;6;5" repeatCount="indefinite"></animate>
+                        </circle>
+                    </g>
+                </svg>
+            </scroller>
         </div>
     </section>
 </div>
@@ -41,21 +87,42 @@
 import {
     getProcuctList
 } from '@/http/index.js'
+import Vue from 'vue'
+import VueScroller from 'vue-scroller';
+Vue.use(VueScroller)
 export default {
     data() {
         return {
             categoryId: this.$route.query.categoryId,
             orderBy: 'default',
-            pageNum: 1,
-            pageSize: 20,
+            pageNum: 0,
+            pageSize: 5,
             productList: []
         }
     },
-    created() {
-        this.fetchProductlist()
+    mounted() {
+        this.$nextTick(() => {
+            this.getHeight()
+        })
     },
     methods: {
-        fetchProductlist() {
+        refresh(done) {
+            console.log('下拉')
+            this.pageNum = 1
+            setTimeout(() => {
+                this.fetchProductList(done)
+            }, 1500)
+        },
+        infinite(done) {
+            console.log('上拉')
+            this.pageNum++
+            console.log(this)
+            setTimeout(() => {
+                console.log(this)
+                this.fetchProductList(done)
+            }, 1500)
+        },
+        fetchProductList(done) {
             const params = {
                 categoryId: this.categoryId,
                 orderBy: this.orderBy,
@@ -63,8 +130,17 @@ export default {
                 pageSize: this.pageSize
             }
             getProcuctList(params).then(res => {
-                // console.log(res)
-                this.productList = res.list
+                if (this.pageNum == 1) {
+                    this.productList = res.list
+                    done()
+                } else {
+                    this.productList = this.productList.concat(res.list)
+                    if (res.list.length == 0) {
+                        done(true)
+                    } else {
+                        done()
+                    }
+                }
             })
         },
         goBack() {
@@ -76,10 +152,16 @@ export default {
                 return
             }
             this.orderBy = orderBy
-            this.fetchProductlist()
+            this.$refs.myScroller.triggerPullToRefresh()
         },
         goDetail(id) {
             this.$router.push('/product/' + id)
+        },
+        getHeight() {
+            let bodyHeight = document.documentElement.clientHeight;
+            let scroller = this.$refs.scroller;
+            let scrollerTop = scroller.getBoundingClientRect().top;
+            scroller.style.height = (bodyHeight - scrollerTop) + "px";
         }
     }
 };
